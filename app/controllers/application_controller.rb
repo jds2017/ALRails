6740520a2be6e9e9210d50addc33d33914a5a3f3
John Tompkins <tompkins.jt@gmail.com>
2017-05-29 23:07:05 -0400

    helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,13 +1,18 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  helper_method :current_user
 
   before_action :require_login
 
   private
 
+  def current_user
+      User.where(username: session[:username]).first
+  end
+
   def require_login
-    unless session[:username]
-      session[:username] = 'jtompkins8'
+    if current_user.nil?
+      session[:username] = User.first.username
     end
   end
 end
