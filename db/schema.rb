281af1ccc74c5c8eba5a23f72e54697cb0b4e7a2
John Tompkins <tompkins.jt@gmail.com>
2017-05-29 21:19:58 -0400

    create userType table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170530000522) do
+ActiveRecord::Schema.define(version: 20170530011854) do
+
+  create_table "user_types", force: :cascade do |t|
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "username"
