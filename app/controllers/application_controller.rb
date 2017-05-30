45fdf6c282a6ff0a798566be017f83826750d8c3
John Tompkins <tompkins.jt@gmail.com>
2017-05-29 22:55:56 -0400

    sort of login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  before_action :require_login
+
+  private
+
+  def require_login
+    unless session[:username]
+      session[:username] = 'jtompkins8'
+    end
+  end
 end
