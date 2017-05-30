c630467a8e1d8ad65f2f61e11f79350bb3e3bc71
John Tompkins <tompkins.jt@gmail.com>
2017-05-30 09:54:06 -0400

    more seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,8 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 User.create username: 'jtompkins8', user_type: 'admin'
+User.create username: 'rsherman7', user_type: 'professor'
+User.create username: 'plester3', user_type: 'professor'
+User.create username: 'kuser', user_type: 'student'
+Course.create title: 'CS 2110', start: '9:05', end: '10:55', user_id: User.where(username: 'rsherman7').first.id
+Course.create title: 'CS 4240', start: '14:05', end: '14:55', user_id: User.where(username: 'plester3').first.id
