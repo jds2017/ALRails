3addd68becc1f1cb30d8bb068437692e85e1a7c1
John Tompkins <tompkins.jt@gmail.com>
2017-05-30 21:53:11 -0400

    Revert "modify answer to have is_correct"
    This reverts commit 5775066c47e729a8604d597620c9be440efa1b13.

--- db/migrate/20170531013921_create_answers.rb
+++ db/migrate/20170531013921_create_answers.rb
@@ -2,7 +2,7 @@ class CreateAnswers < ActiveRecord::Migration[5.1]
   def change
     create_table :answers do |t|
       t.string :answer
-      t.boolean :is_correct
+
       t.timestamps
     end
   end
