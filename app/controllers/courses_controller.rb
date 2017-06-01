01e3fdcbfd7d8b69b08d47d215e12f9aa25ca2b1
John Tompkins <tompkins.jt@gmail.com>
2017-05-31 20:12:13 -0400

    validations for courses

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,10 +1,17 @@
 class CoursesController < ApplicationController
   before_action :set_course, only: [:show, :edit, :update, :destroy]
+  before_action :validate_is_prof, only: [:create]
+  before_action :validate_is_owner, only: [:edit, :update, :destroy]
+  before_action :validate_is_registered, only: [:show]
 
   # GET /courses
   # GET /courses.json
   def index
-    @courses = Course.all
+    if current_user.is_admin
+      @courses = Course.all
+    else
+      @courses = current_user.courses
+    end
   end
 
   # GET /courses/1
@@ -62,6 +69,24 @@ class CoursesController < ApplicationController
   end
 
   private
+    def validate_is_prof
+      if !current_user.is_admin and !current_user.is_professor
+        raise
+      end
+    end
+
+    def validate_is_owner
+      if !current_user.courses_as_professor.include?(@course) && !current_user.courses_as_assistant.include?(@course)
+        raise
+      end
+    end
+
+    def validate_is_registered
+      if !current_user.courses.include? @course
+        raise
+      end
+    end
+
     # Use callbacks to share common setup or constraints between actions.
     def set_course
       @course = Course.find(params[:id])
