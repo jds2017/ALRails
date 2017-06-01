c0af3476fc273c776b2eaa90222bda9fd70897a0
John Tompkins <tompkins.jt@gmail.com>
2017-05-31 20:21:54 -0400

    readable form

--- app/models/course.rb
+++ app/models/course.rb
@@ -3,4 +3,8 @@ class Course < ApplicationRecord
   has_many :users, through: :registrations
 
   validates :semester, inclusion: { in: %w(SUMMER FALL SPRING) }
+
+  def to_s
+    self.name
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,14 @@ class User < ApplicationRecord
   has_many :responses
 
   def courses_as_assistant
-      Registration.where(user: self, role: 'ASSISTANT').map { |r| r.course }
+    Registration.where(user: self, role: 'ASSISTANT').map { |r| r.course }
   end
 
   def courses_as_professor
-      Registration.where(user: self, role: 'PROFESSOR').map { |r| r.course }
+    Registration.where(user: self, role: 'PROFESSOR').map { |r| r.course }
+  end
+
+  def to_s
+    self.username
   end
 end
