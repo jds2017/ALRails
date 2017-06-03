6f7bbcf8a5475a6c9b9a864bbc08382015b1a1d6
John Tompkins <tompkins.jt@gmail.com>
2017-06-03 11:25:35 -0400

    question to reference course

--- app/models/course.rb
+++ app/models/course.rb
@@ -4,6 +4,10 @@ class Course < ApplicationRecord
 
   validates :semester, inclusion: { in: %w(SUMMER FALL SPRING) }
 
+  def questions
+    Question.where(course_name: self.name)
+  end
+
   def to_s
     self.name
   end
--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -1,7 +1,7 @@
 require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "reads questions" do
+    assert_equal 2, Course.where(name: 'CourseOne').first.questions.size()
+  end
 end
