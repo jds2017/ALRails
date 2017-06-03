5de517eea4c32eba09baa73b5578f6ca1a284c03
John Tompkins <tompkins.jt@gmail.com>
2017-06-03 13:50:32 -0400

    use has_many

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,12 +1,9 @@
 class Course < ApplicationRecord
   has_many :registrations
   has_many :users, through: :registrations
+  has_many :questions, foreign_key: 'course_name', primary_key: 'name'
 
   validates :semester, inclusion: { in: %w(SUMMER FALL SPRING) }
-
-  def questions
-    Question.where(course_name: self.name)
-  end
 
   def to_s
     self.name
