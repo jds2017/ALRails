0ce68485f83c2e018538e0425b617f0180b82668
John Tompkins <tompkins.jt@gmail.com>
2017-06-03 23:46:43 -0400

    answers in question form

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,5 +1,6 @@
 class Question < ApplicationRecord
   has_many :answers
+  accepts_nested_attributes_for :answers, allow_destroy: true, reject_if: lambda {|a| a['answer'].blank? }
 
   has_many :question_set_junctions
   has_many :questions, through: :question_set_junctions
