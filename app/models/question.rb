9336a74a6a30b37bd88fe469983f18d7a29b4797
John Tompkins <tompkins.jt@gmail.com>
2017-06-06 21:13:14 -0400

    add some more tests

--- app/models/question.rb
+++ app/models/question.rb
@@ -4,4 +4,8 @@ class Question < ApplicationRecord
 
   has_many :question_set_junctions
   has_many :questions, through: :question_set_junctions
+
+  def correct_answers
+    answers.find_all { |a| a.is_correct }
+  end
 end
--- test/system/questions_test.rb
+++ test/system/questions_test.rb
@@ -1,9 +1,25 @@
 require "application_system_test_case"
 
 class QuestionsTest < ApplicationSystemTestCase
-  # test "visiting the index" do
-  #   visit questions_url
-  #
-  #   assert_selector "h1", text: "Question"
-  # end
+  test "answers get saved with questions" do
+    visit '/questions?un=john'
+    click_on 'New Question'
+    fill_in 'Body', with: 'yesorno'
+    find('#question_answers_attributes_0_answer').set 'yes'
+    find('#question_answers_attributes_1_answer').set 'no'
+    find('#question_answers_attributes_0_is_correct').set(true)
+    click_on 'Create Question'
+    assert_text 'Question was successfully created.'
+    click_on 'Edit'
+    assert_equal 1, Question.find_by(body: 'yesorno').correct_answers.size
+    assert_equal 'yes', Question.find_by(body: 'yesorno').correct_answers.first.answer
+    assert_equal 5, Question.find_by(body: 'yesorno').answers.size
+    find('#question_answers_attributes_0_is_correct').set(false)
+    find('#question_answers_attributes_1_is_correct').set(true)
+    click_on 'Update Question'
+    assert_text 'yesorno'
+    assert_equal 1, Question.find_by(body: 'yesorno').correct_answers.size
+    assert_equal 'no', Question.find_by(body: 'yesorno').correct_answers.first.answer
+    assert_equal 5, Question.find_by(body: 'yesorno').answers.size
+  end
 end
