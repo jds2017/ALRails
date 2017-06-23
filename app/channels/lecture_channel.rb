b24c575a1cd0e0bb809efcef92ae501f3a7bee75
John Tompkins <tompkins.jt@gmail.com>
2017-06-22 20:10:10 -0400

    student streams the question too

--- app/channels/lecture_channel.rb
+++ app/channels/lecture_channel.rb
@@ -1,12 +1,8 @@
 class LectureChannel < ApplicationCable::Channel
-  @teacher = nil
   @lecture = nil
 
   def subscribed
-    if current_user.is_professor
-      @teacher = current_user
-      stream_for @teacher
-    end
+    stream_for current_user
     @lecture = Lecture.find(params[:lecture])
     stream_for @lecture
   end
@@ -17,6 +13,9 @@ class LectureChannel < ApplicationCable::Channel
     end
     if ("join" == data['msg'])
       LectureChannel.broadcast_to(@lecture, {'msg' => 'join', 'user' => data['user']})
+    end
+    if ("question" == data['msg'])
+      LectureChannel.broadcast_to(@lecture, {'msg' => 'question', 'body' => data['body']})
     end
   end
 end
