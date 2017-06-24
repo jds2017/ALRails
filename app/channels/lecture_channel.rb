e2aa73e872ba48e4689ffe4e6ab729a4097821b2
John Tompkins <tompkins.jt@gmail.com>
2017-06-23 23:35:17 -0400

    restrict some communication to professor

--- app/channels/lecture_channel.rb
+++ app/channels/lecture_channel.rb
@@ -4,6 +4,9 @@ class LectureChannel < ApplicationCable::Channel
   def subscribed
     @lecture = Lecture.find(params[:lecture])
     stream_for @lecture
+    if current_user.is_professor
+      stream_for "leader_#{@lecture.id}"
+    end
   end
 
   def unsubscribed
@@ -12,17 +15,17 @@ class LectureChannel < ApplicationCable::Channel
 
   def receive(data)
     if ("requestQuestionSet" == data['msg'])
-      LectureChannel.broadcast_to(@lecture, {'msg' => 'helpme', 'qs' => @lecture.question_set.as_json(include: { questions: {include: {answers: {except: :is_correct}}}})})
+      LectureChannel.broadcast_to("leader_#{@lecture.id}", {'msg' => 'helpme', 'qs' => @lecture.question_set.as_json(include: { questions: {include: {answers: {except: :is_correct}}}})})
     end
     if ("join" == data['msg'])
-      LectureChannel.broadcast_to(@lecture, {'msg' => 'join', 'user' => data['user']})
+      LectureChannel.broadcast_to("leader_#{@lecture.id}", {'msg' => 'join', 'user' => data['user']})
     end
     if ("question" == data['msg'])
       LectureChannel.broadcast_to(@lecture, {'msg' => 'question', 'body' => data['body']})
     end
     if ("response" == data['msg'])
       Response.create! lecture: @lecture, user: current_user, question_id: data['question'].to_i, answer_id: data['answer'].to_i
-      LectureChannel.broadcast_to(@lecture, {'msg' => 'answer', 'answer' => data['answer']})
+      LectureChannel.broadcast_to("leader_#{@lecture.id}", {'msg' => 'answer', 'answer' => data['answer']})
     end
   end
 end
