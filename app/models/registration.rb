59a0873f52b9e90e0f64126fb727a74b0e2ac04e
Jensen <jensptan@gmail.com>
2017-06-29 19:30:38 -0700

    ASSISTANT covers both professor and TA roles in registrations

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -2,5 +2,5 @@ class Registration < ApplicationRecord
   belongs_to :user
   belongs_to :course
 
-  validates :role, inclusion: { in: %w(STUDENT ASSISTANT PROFESSOR) }
+  validates :role, inclusion: { in: %w(STUDENT ASSISTANT) }
 end
