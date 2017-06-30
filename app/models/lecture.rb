af68add19be1dbadaa517eee27e23216320fadf0
kreatz-gatech <bpulliam96@gmail.com>
2017-06-30 14:58:09 -0400

    Added question statistics method
    Given a question, generates a hash keyed by answer_id with values corresponding to how many responses were received for that answer.

--- app/models/lecture.rb
+++ app/models/lecture.rb
@@ -1,3 +1,16 @@
 class Lecture < ApplicationRecord
   belongs_to :question_set
+  
+  #compute out of all the responses, how many corresponded to each answer
+  #returns hash keyed by answer id with value for % responses with that answer id
+  def self.compute_percentages (q)
+    @responses = Response.where(question: q)
+    @answers = Answer.where(question: q)
+    @total = @responses.length
+    hash = Hash.new(0)
+    @answers.each{|key| hash[key.id] = 0}
+    @responses.each{|key| hash[key.answer_id] += 1}
+    hash.each {|key, value| hash[key] = (value.to_f / @total) }
+    return hash
+  end
 end
