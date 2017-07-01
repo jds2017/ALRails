c7d3e8e525fd86173ae093facf9716b77ce8b44a
John Tompkins <tompkins.jt@gmail.com>
2017-07-01 16:11:02 -0400

    bug actually fixed now

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,7 +2,7 @@ require 'json'
 
 class Question < ApplicationRecord
   has_many :answers
-  accepts_nested_attributes_for :answers, allow_destroy: true
+  accepts_nested_attributes_for :answers, allow_destroy: true, :reject_if => proc { |a| a[:answer].blank? }
 
   has_many :question_set_junctions
 
