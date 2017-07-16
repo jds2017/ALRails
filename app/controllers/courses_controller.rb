f48eb910007c761a359b92804ee3b37f9f962a5f
John Tompkins <tompkins.jt@gmail.com>
2017-07-16 15:04:07 -0400

    remove admin logic here

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,17 +1,11 @@
 class CoursesController < ApplicationController
   before_action :find_course, only: [:show, :edit, :update, :destroy]
   before_action :new_course, only: [:create]
-  before_action :validate_read, only: [:show, :edit]
-  before_action :validate_modify, only: [:create, :destroy, :update]
 
   # GET /courses
   # GET /courses.json
   def index
-    if current_user.is_admin
-      @courses = Course.all
-    else
-      @courses = current_user.courses
-    end
+    @courses = current_user.courses
   end
 
   # GET /courses/1
@@ -72,14 +66,6 @@ class CoursesController < ApplicationController
   end
 
   private
-    def validate_read
-      raise unless current_user.is_admin || current_user.courses.include?(@course)
-    end
-
-    def validate_modify
-      raise unless current_user.is_admin || current_user.is_professor
-    end
-
     # Use callbacks to share common setup or constraints between actions.
     def find_course
       @course = Course.find(params[:id])
