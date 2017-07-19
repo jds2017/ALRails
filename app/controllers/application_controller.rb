822a7a80194ae27907baf1e9cc9826dec1dc2de5
jacobahayes <jakehayes82@gmail.com>
2017-07-19 19:44:00 -0400

    Adds checks for environment so that gatech login only happens in production.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,7 @@
 class ApplicationController < ActionController::Base
-  before_action CASClient::Frameworks::Rails::Filter
+  if Rails.env.production?
+    before_action CASClient::Frameworks::Rails::Filter
+  end
   protect_from_forgery with: :exception
   helper_method :current_user
 
@@ -8,10 +10,14 @@ class ApplicationController < ActionController::Base
   private
 
   def current_user
+    if Rails.env.production?
       if User.find_by(username: session[:cas_user]).nil?
-          User.create username: session[:cas_user]
+        User.create username: session[:cas_user]
       end
       User.find_by(username: session[:cas_user])
+    else
+      User.find_by(username: session[:username])
+    end
   end
 
   def require_login
