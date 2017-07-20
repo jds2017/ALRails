5c098f500291bf84ef4f5b059ec5b2cde39d742f
John Tompkins <tompkins.jt@gmail.com>
2017-07-19 22:15:26 -0400

    lecture work when hosted

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Activelearning
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    Rails.application.config.action_cable.allowed_request_origins = [/localhost/, /popquiz.cc.gatech.edu/]
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
