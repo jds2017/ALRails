5842dd6329213a9f86fe960b36e976b5e2f02e84
John Tompkins <tompkins.jt@gmail.com>
2017-07-20 10:54:14 -0400

    fix tests

--- test/models/question_test.rb
+++ test/models/question_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 class QuestionTest < ActiveSupport::TestCase
   test "reads questions" do
-    assert_equal 2, Course.find_by(name: 'CourseOne').questions.size()
+    assert_equal 2, Course.find_by(name: 'XX9999').questions.size()
   end
   test "finds correct answers" do
-    assert_equal 1, Question.find_by(body: Course.find_by(name: 'CourseOne').questions[1].body).correct_answers.size()
+    assert_equal 1, Question.find_by(body: Course.find_by(name: 'XX9999').questions[1].body).correct_answers.size()
   end
 end
