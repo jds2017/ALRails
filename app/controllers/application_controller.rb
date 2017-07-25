8c2774900d37dccddb7b33f3eb4fd1161147198c
John Tompkins <tompkins.jt@gmail.com>
2017-07-25 18:19:19 -0400

    more refactor

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,32 +4,32 @@ class ApplicationController < ActionController::Base
   else
     before_action :development_login
   end
+  before_action :create_user
   protect_from_forgery with: :exception
   helper_method :current_user
 
   private
 
   def current_user
-    if Rails.env.production?
-      if User.find_by(username: session[:cas_user]).nil?
-        User.create username: session[:cas_user]
-      end
-      User.find_by(username: session[:cas_user])
-    else
-      User.find_by(username: session[:username])
-    end
+    User.find_by(username: session[:cas_user])
   end
 
   def development_login
     if params['un']
-      cookies.signed[:username] = params['un']
-      session[:username] = params['un']
+      session[:cas_user] = params['un']
     else
       if Rails.env == 'test'
-        session[:username] = User.first.username
+        session[:cas_user] = User.find_by(is_admin: true).username
       else
-        session[:username] = User.find_by(username: 'rkalhan4').username
+        session[:cas_user] = User.find_by(username: 'rkalhan4').username
       end
+    end
+  end
+
+  def create_user
+    cookies.signed[:username] = session[:cas_user]
+    if User.find_by(username: session[:cas_user]).nil?
+      User.create username: session[:cas_user]
     end
   end
 end
